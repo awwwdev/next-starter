500d8f49b039e4951d8d968c705083e34c49bc8e
yourname <example@example.com>
2023-01-24 19:16:57 -0800

    fix: add prettier and eslint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,45 +1,38 @@
-import type { AppProps } from 'next/app'
-import '@unocss/reset/sanitize/sanitize.css'
-import '@unocss/reset/tailwind.css'
+import type { AppProps } from "next/app";
+import "@unocss/reset/sanitize/sanitize.css";
+import "@unocss/reset/tailwind.css";
 import "@/styles/reset.css";
-import 'uno.css'
+import "uno.css";
 import "@/styles/globals.css";
 
-import { QueryClientProvider } from '@tanstack/react-query';
-import { QueryClient } from '@tanstack/react-query';
-import { ThemeProvider } from 'next-themes'
-import ThemeToggler from '@/components/ThemeToggler';
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ThemeProvider } from "next-themes";
+import ThemeToggler from "@/components/ThemeToggler";
 
-import Head from 'next/head'; 
-import Icon from '@/components/Icon';
-import { useState } from 'react';
-import Link from 'next/link';
-import { Inter } from '@next/font/google';
+import Head from "next/head";
+import Link from "next/link";
+import { Inter } from "@next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
-
+const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-
   const queryClient = new QueryClient({
     defaultOptions: {
       queries: {
         retry: 0,
         refetchOnMount: false,
         refetchOnWindowFocus: false,
-      }
-    }
+      },
+    },
   });
 
+  const a = "ff";
 
   return (
     <>
       <Head>
         <meta property="og:title" content="" />
-        <meta
-          property="og:description"
-          content="About my website in one sentence"
-        />
+        <meta property="og:description" content="About my website in one sentence" />
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width" />
         <meta property="og:site_name" content="my-site0name" />
@@ -47,28 +40,32 @@ export default function App({ Component, pageProps }: AppProps) {
       </Head>
       <QueryClientProvider client={queryClient}>
         <ThemeProvider attribute="class">
-          <div className={`${inter.className} h-full grid`} style={{gridTemplateRows: 'auto 1fr auto'}}>
-            <header> 
-              <nav className='mx-auto max-w-page py-3 flex justify-between'>
-                <Link href='/' className=' fw-900'>LOGO</Link>
-                <ThemeToggler className=''  />
-              </nav>         
+          <div className={`${inter.className} h-full grid`} style={{ gridTemplateRows: "auto 1fr auto" }}>
+            <header>
+              <nav className="mx-auto max-w-page py-3 flex justify-between">
+                <Link href="/" className=" fw-900">
+                  LOGO
+                </Link>
+                <ThemeToggler className="" />
+              </nav>
             </header>
-           <main >
+            <main>
               <Component {...pageProps} />
-          </main>
-          <footer className='bg-gray2'>
-            <div className='mx-auto max-w-page c-gray11 pb-3'>
-              <div className='flex ac jc py-3'> <Link className='' href='/'>LOGO</Link></div>
-              <div className='flex jc ac'>
-                This is the footer
+            </main>
+            <footer className="bg-gray2">
+              <div className="mx-auto max-w-page c-gray11 pb-3">
+                <div className="flex ac jc py-3">
+                  {" "}
+                  <Link className="" href="/">
+                    LOGO
+                  </Link>
+                </div>
+                <div className="flex jc ac">This is the footer</div>
               </div>
-            </div>
-          </footer>
+            </footer>
           </div>
         </ThemeProvider>
       </QueryClientProvider>
     </>
-  )
-
+  );
 }
